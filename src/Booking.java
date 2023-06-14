fb8bce1c05ac6b70eb7bc3cc10265d748382d096
Petr Svoboda <svob.petr@seznam.cz>
2023-06-14 10:58:20 +0200

    předelano, tisk pouze prazdné zavorky, zkuseni pridat add - cerveny porád nefunguje

--- src/Booking.java
+++ src/Booking.java
@@ -7,39 +7,28 @@ import java.math.BigDecimal;
 
 
 public class Booking {
-    public static void main(String... args){//pokud nebudu tisknout tak odendat
-    Rooms room;  //odendal jsem private a getry a setry, který v Listu nemají význam
-     Guest guest;
+  //  public static void main(String... args){//pokud nebudu tisknout tak odendat
+   private Rooms room;  //odendal jsem private a getry a setry, který v Listu nemají význam
+    private Guest guest;
 
-        {
+
+
      List<Guest> GuestList = new ArrayList<>();
+    // public void add(Guest new Guest){  //dal jsem bokem, při vypisu bylo červeny
+    // GuestList.add(new Guest)}
+
+    List<Rooms>RoomsList=new ArrayList<>();
+    public void add(Rooms newRooms){
+        RoomsList.add(newRooms);
+
+
     //Guest Guest1=new Guest("Adela Malikova","1990.13.03.",true);
     // Guest Guest2= new Guest("Jan Dvoracek","1995.05.05.",false);//pridal jsem konstruktor s pokojem
 
     //pomoci adelaName tam nedostanu set.Name. chce to konstruktor, co s tím?
 
-    Guest Guest1 = new Guest("Adela Malikova", "1990.13.03.", true, 1, LocalDate.of(2021, 7, 19), LocalDate.of(2021, 7, 26));
-    Guest Guest2 = new Guest("Jan Dvoracek", "1995.05.05.", false, 3); //chci oba dva
-    Guest Guest3 = new Guest("Adela Malikova" + "Jan Dvoracek", "1990.13.03." + "1995.05.05.", false, 1, LocalDate.of(2021, 9, 1), LocalDate.of(2021, 9, 14));
-    //třidu rezervace ted nepotřebuji
 
-
-
-
-     System.out.println(GuestList);
-        for(Guest Guest:GuestList){
-        Guest.setName(String.valueOf(Guest1));//chtělo to String.valueOf, jinak cerveně podrzený Guest1, tak jsem to tam dal
-            Guest.setNarozeni(String.valueOf(Guest1));
-            System.out.println(Guest.getName()+"narozen(a)"+Guest.getNarozeni()); }
-            //mělo by to vypsat, jenze Guest1 je podrzeny, metoda toString vracela pochybně Main, tak jsem zkusil takto
-            System.out.println(GuestList);
-            for(Guest Guest:GuestList){
-                Guest.setName(""+Guest2);
-                Guest.setNarozeni(""+Guest2);
-                System.out.println(Guest.getName()+"narozen"+Guest.getNarozeni());
-            }//druhý zpusob převodu na text, asi odkazuju na něco jineho v ArrayListu
-
-}}}
+}}
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -12,15 +12,38 @@ public class Main {
       // boolean Guest2;
 
         List<Rooms>RoomsList=new ArrayList<>();
-        Rooms Rooms1=new Rooms(1,1,true,true, BigDecimal.valueOf(1000));
+        Rooms.add(new Rooms(1, 1, true, true, BigDecimal.valueOf(1000)));
         Rooms Rooms2=new Rooms(2,1,true,true,BigDecimal.valueOf(1000));
         Rooms Rooms3=new Rooms(3,3,false,true,BigDecimal.valueOf(2400));
 
 
         System.out.println(RoomsList);
 
+        List<Guest> GuestList = new ArrayList<>();
+        Guest Guest1 = new Guest("Adela Malikova", "1990.13.03.", true, 1, LocalDate.of(2021, 7, 19), LocalDate.of(2021, 7, 26));
+        Guest Guest2 = new Guest("Jan Dvoracek", "1995.05.05.", false, 3); //chci oba dva
+        Guest Guest3 = new Guest("Adela Malikova" + "Jan Dvoracek", "1990.13.03." + "1995.05.05.", false, 1, LocalDate.of(2021, 9, 1), LocalDate.of(2021, 9, 14));
+        //třidu rezervace ted nepotřebuji
 
+
+
+
+        System.out.println(GuestList);
+        for(Guest Guest:GuestList){
+            Guest.setName(String.valueOf(Guest1));//chtělo to String.valueOf, jinak cerveně podrzený Guest1, tak jsem to tam dal
+            Guest.setNarozeni(String.valueOf(Guest1));
+            System.out.println(Guest.getName()+"narozen(a)"+Guest.getNarozeni()); }
+        //mělo by to vypsat, jenze Guest1 je podrzeny, metoda toString vracela pochybně Main, tak jsem zkusil takto
+        System.out.println(GuestList);
+        for(Guest Guest:GuestList){
+            Guest.setName(""+Guest2);
+            Guest.setNarozeni(""+Guest2);
+            System.out.println(Guest.getName()+"narozen"+Guest.getNarozeni());
         }//druhý zpusob převodu na text, asi odkazuju na něco jineho v ArrayListu
+
+
+
+    }//druhý zpusob převodu na text, asi odkazuju na něco jineho v ArrayListu
       //  RoomsList.clear();}//měl by se vymazat Rooms3, nejde zapsat -cervene,ale podrzeno a RoomList vypiše jen zavorky
 
 
